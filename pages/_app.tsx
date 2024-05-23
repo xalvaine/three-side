ceb80b9ac9255922c5725455bb2e8958297c01a3
xalvaine <xalvaine@gmail.com>
2024-05-24 00:53:42 +0300

    [fix] Fix vercel build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,6 @@
 import 'app/globals.scss'
 import type { AppProps } from 'next/app'
 
-import { Rubik } from 'next/font/google'
-
-const rubik = Rubik({ subsets: ['latin', 'cyrillic'] })
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <style jsx global>{`
-        html {
-          font-family: ${rubik.style.fontFamily};
-        }
-      `}</style>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
