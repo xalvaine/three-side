a936fb1558bba53757b104a946655402f47c165c
xalvaine <xalvaine@gmail.com>
2024-05-24 21:13:33 +0300

    [feat] Add pipeline steps

--- src/pages/home/ui/pipeline/pipeline.tsx
+++ src/pages/home/ui/pipeline/pipeline.tsx
@@ -1,11 +1,56 @@
-import { Title } from 'pages/home/ui/title/title'
+import { Title } from '../title/title'
+
+import styles from './pipeline.module.scss'
+import classNames from 'classnames'
+import { useState } from 'react'
+
+const steps: { title: string; text: string }[] = [
+  {
+    title: `Консультация`,
+    text: `Поможем определить именно ту услугу, которая в первую очередь необходима бизнесу. Возьмем на себя составление ТЗ. NDA.`,
+  },
+  {
+    title: `Выбор исполнителя`,
+    text: `Предложим 3-5 исполнителей с релевантным опытом. Каждый исполнитель указывает свою цену и срок. Выбор остается за вами.`,
+  },
+  {
+    title: `Оформление документов`,
+    text: `Вы подписываете два контракта: с исполнителем и площадкой. Условия в договорах прозрачны и учитывают интересы всех сторон.`,
+  },
+  {
+    title: `Выполнение работ`,
+    text: `Исполнитель сообщает о статусе работ в проектном чате, где вы можете задать вопросы и получить объяснения технических аспектов на языке бизнеса.`,
+  },
+  {
+    title: `Итоговый отчет`,
+    text: `Вы получаете отчет исполнителя и сертификат 3side, подтверждающий достаточный уровень проверки и общее качество работы. `,
+  },
+]
 
 export const Pipeline = () => {
+  const [activeIndex, setActiveIndex] = useState(0)
+
   return (
     <>
       <Title>
         Выстроим прозрачный процесс, чтобы вы быстрее получали результаты
       </Title>
+      <ul className={styles.steps}>
+        {steps.map(({ title, text }, index) => (
+          <li
+            key={title}
+            className={classNames(
+              styles.step,
+              index === activeIndex && styles.stepActive,
+            )}
+            onClick={() => setActiveIndex(index)}
+          >
+            <p className={styles.stepOrder}>{index + 1}</p>
+            <h3 className={styles.stepTitle}>{title}</h3>
+            <p className={styles.stepText}>{text}</p>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
