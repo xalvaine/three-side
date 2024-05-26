5ea0f684355ecf4ed0e3d8c13a3e68186efb50ae
xalvaine <xalvaine@gmail.com>
2024-05-26 20:04:32 +0300

    [fix] Fix safari shit

--- src/widgets/pipeline/ui/performer-selection/performer-selection.tsx
+++ src/widgets/pipeline/ui/performer-selection/performer-selection.tsx
@@ -10,14 +10,22 @@ export const PerformerSelection = () => {
     <PerformerSelectionDesktop className={styles.image} />
   ) : (
     <div className={styles.wrapper}>
-      <p className={styles.text}>
-        Опыт работы по конкретной специализации с описанием проектов
-      </p>
-      <p className={styles.text}>
-        Образование, международные сертификаты участие в профильных
-        соревнованиях
-      </p>
-      <p className={styles.text}>Заказчики из известных  компаний</p>
+      <div className={styles.texts}>
+        <p className={styles.text}>
+          О&shy;пыт ра&shy;бо&shy;ты по кон&shy;крет&shy;ной
+          спе&shy;ци&shy;а&shy;ли&shy;за&shy;ци&shy;и с
+          о&shy;пи&shy;са&shy;ни&shy;ем про&shy;ек&shy;тов
+        </p>
+        <p className={styles.text}>
+          Об&shy;ра&shy;зо&shy;ва&shy;ни&shy;е,
+          меж&shy;ду&shy;на&shy;род&shy;ны&shy;е сер&shy;ти&shy;фи&shy;ка&shy;ты
+          у&shy;ча&shy;сти&shy;е в про&shy;филь&shy;ных
+          со&shy;рев&shy;но&shy;ва&shy;ни&shy;ях
+        </p>
+        <p className={styles.text}>
+          За&shy;каз&shy;чи&shy;ки из из&shy;вест&shy;ных ком&shy;па&shy;ний
+        </p>
+      </div>
       <PerformerSelectionMobile className={styles.image} />
     </div>
   )
