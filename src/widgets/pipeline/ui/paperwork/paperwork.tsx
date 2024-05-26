3a380a61a18bfa76f3da702d72a900958250fd51
xalvaine <xalvaine@gmail.com>
2024-05-26 19:46:05 +0300

    [feat] Finish everything

--- src/widgets/pipeline/ui/paperwork/paperwork.tsx
+++ src/widgets/pipeline/ui/paperwork/paperwork.tsx
@@ -1,3 +1,20 @@
+import PaperWorkIcon from '../../assets/paperwork.svg'
+
+import styles from './paperwork.module.scss'
+
 export const Paperwork = () => {
-  return <></>
+  return (
+    <div className={styles.wrapper}>
+      <p className={styles.name}>3side</p>
+      <span />
+      <p className={styles.name}>Исполнитель</p>
+      <PaperWorkIcon className={styles.image} />
+      <p className={styles.label}>$ комиссия площадки </p>
+      <p className={styles.label}>Заказчик</p>
+      <p className={styles.label}>
+        $ фик&shy;си&shy;р&shy;ован&shy;но&shy;е
+        воз&shy;на&shy;граж&shy;де&shy;ни&shy;е
+      </p>
+    </div>
+  )
 }
--- src/widgets/pipeline/ui/performer-selection/performer-selection.tsx
+++ src/widgets/pipeline/ui/performer-selection/performer-selection.tsx
@@ -1,3 +1,24 @@
+import PerformerSelectionMobile from '../../assets/performer-selection-mobile.svg'
+import PerformerSelectionDesktop from '../../assets/performer-selection-desktop.svg'
+
+import styles from './performer-selection.module.scss'
+import { useIsDesktop } from 'shared/lib'
+
 export const PerformerSelection = () => {
-  return <></>
+  const { isDesktop, isWindow } = useIsDesktop()
+  return !isWindow || isDesktop ? (
+    <PerformerSelectionDesktop className={styles.image} />
+  ) : (
+    <div className={styles.wrapper}>
+      <p className={styles.text}>
+        Опыт работы по конкретной специализации с описанием проектов
+      </p>
+      <p className={styles.text}>
+        Образование, международные сертификаты участие в профильных
+        соревнованиях
+      </p>
+      <p className={styles.text}>Заказчики из известных  компаний</p>
+      <PerformerSelectionMobile className={styles.image} />
+    </div>
+  )
 }
